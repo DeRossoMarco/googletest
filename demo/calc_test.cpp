899cfab46a32f7dcb5ec809bad8978f5e2693995
Marco De Rosso <100926879+DeRossoMarco@users.noreply.github.com>
2023-09-27 00:56:40 +0200

    Update calc_test.cpp

--- demo/calc_test.cpp
+++ demo/calc_test.cpp
@@ -2,15 +2,15 @@
 #include "calc.hpp"
 
 TEST(CalcTest, Addition) {
-    ASSERT_EQ(2, add(1, 1));
-    ASSERT_EQ(5, add(3, 2));
-    ASSERT_EQ(10, add(7, 3));
+    EXPECT_EQ(2, add(1, 1));
+    EXPECT_EQ(5, add(3, 2));
+    EXPECT_EQ(10, add(7, 3));
 }
 
 TEST(CalcTest, Subtraction) {
-    ASSERT_EQ(3, sub(5, 2));
-    ASSERT_EQ(-10, sub(5, 15));
-    ASSERT_EQ(0, sub(5, 5));
+    EXPECT_EQ(3, sub(5, 2));
+    EXPECT_EQ(-10, sub(5, 15));
+    EXPECT_EQ(0, sub(5, 5));
 }
 
 int main(int argc, char **argv) {
