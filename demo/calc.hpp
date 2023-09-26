6b0461ccf0a941e9f753ec7047d8c44d05e59d06
Marco De Rosso <100926879+DeRossoMarco@users.noreply.github.com>
2023-09-27 00:39:54 +0200

    Update calc.hpp

--- demo/calc.hpp
+++ demo/calc.hpp
@@ -1,12 +1,8 @@
 #ifndef CALC_HPP_
 #define CALC_HPP_
 
-double add(double x, double y);
+int add(int x, int y);
 
-double sub(double x, double y);
-
-double mul(double x, double y);
-
-double div(double x, double y);
+int sub(int x, int y);
 
 #endif
