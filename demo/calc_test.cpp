e2ef3de6cf7640bd25ae31a73bbf2c2bc7cdcb50
Marco De Rosso <100926879+DeRossoMarco@users.noreply.github.com>
2023-09-27 00:40:14 +0200

    Update calc_test.cpp

--- demo/calc_test.cpp
+++ demo/calc_test.cpp
@@ -13,20 +13,6 @@ TEST(CalcTest, Subtraction) {
     ASSERT_EQ(0, sub(5, 5));
 }
 
-TEST(CalcTest, Multiplication) {
-    ASSERT_EQ(10, mul(5, 2));
-    ASSERT_EQ(10, mul(2, 5));
-    ASSERT_EQ(-6, mul(-3, 2));
-    ASSERT_EQ(9, mul(-3, -3));
-}
-
-TEST(CalcTest, Division) {
-    ASSERT_EQ(5, div(10, 2));
-    ASSERT_EQ(0.5, div(2, 4));
-    ASSERT_EQ(-3, div(6, -2));
-    ASSERT_EQ(1, div(-3, -3));
-}
-
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
