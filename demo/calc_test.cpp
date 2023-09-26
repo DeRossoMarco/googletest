ba450cedd8632e6433885effd74c3eb50c536055
Marco De Rosso <100926879+DeRossoMarco@users.noreply.github.com>
2023-09-27 00:31:27 +0200

    Update calc_test.cpp

--- demo/calc_test.cpp
+++ demo/calc_test.cpp
@@ -2,29 +2,29 @@
 #include "calc.hpp"
 
 TEST(CalcTest, Addition) {
-    EXPECT_EQ(2, add(1, 1));
-    EXPECT_EQ(5, add(3, 2));
-    EXPECT_EQ(10, add(7, 3));
+    ASSERT_EQ(2, add(1, 1));
+    ASSERT_EQ(5, add(3, 2));
+    ASSERT_EQ(10, add(7, 3));
 }
 
 TEST(CalcTest, Subtraction) {
-    EXPECT_EQ(3, sub(5, 2));
-    EXPECT_EQ(-10, sub(5, 15));
-    EXPECT_EQ(0, sub(5, 5));
+    ASSERT_EQ(3, sub(5, 2));
+    ASSERT_EQ(-10, sub(5, 15));
+    ASSERT_EQ(0, sub(5, 5));
 }
 
 TEST(CalcTest, Multiplication) {
-    EXPECT_EQ(10, mul(5, 2));
-    EXPECT_EQ(10, mul(2, 5));
-    EXPECT_EQ(-6, mul(-3, 2));
-    EXPECT_EQ(9, mul(-3, -3));
+    ASSERT_EQ(10, mul(5, 2));
+    ASSERT_EQ(10, mul(2, 5));
+    ASSERT_EQ(-6, mul(-3, 2));
+    ASSERT_EQ(9, mul(-3, -3));
 }
 
 TEST(CalcTest, Division) {
-    EXPECT_EQ(5, div(10, 2));
-    EXPECT_EQ(0.5, div(2, 4));
-    EXPECT_EQ(-3, div(6, -2));
-    EXPECT_EQ(1, div(-3, -3));
+    ASSERT_EQ(5, div(10, 2));
+    ASSERT_EQ(0.5, div(2, 4));
+    ASSERT_EQ(-3, div(6, -2));
+    ASSERT_EQ(1, div(-3, -3));
 }
 
 int main(int argc, char **argv) {
